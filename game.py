f44a6d9c516d8a4a39d81ea906c0c7dc008b2e70
Erick Valencia <evalencia0911@gmail.com>
2024-03-07 13:52:35 -0700

    mowing the lawn with teeth forever

--- game.py
+++ game.py
@@ -15,29 +15,53 @@ equipment = [
     {"name": "Teeth", "price": 0, "money": 1}
 ]
 
-# Create the character CLASS
+# Create the character CLASS for the player
+class Character:
 
     # define constructor
+    def __init__(self, name):
+        
+        # define your name
+        self.name = name
+        self.money = 0
+    def mow(self, lawn):
+        self.money += 1
+        print(f"{self.name} has mowed 1 lawn and received ${self.money}")
 
-    # define the action of mowing the lawn
-        # What do you earn from mowing the lawn and where does it tabulate your earnings
+# Create a character CLASS for the lawn
+class Lawn:
+
+    # define constructor
+    def __init__(self):
+        self.money = 1
 
 # General functions
+        
+# mowing function
+
 
     # Win condition
 
-    # Game set up 
+# Game set up 
+player_name = input("Welcome to the mow gang, what is your name")
+
+# take Character class and create a player
+player = Character(player_name)
+
+lawn = Lawn()
 
 # Loop for the game
 
     # Input loops
 while(True):
     # text for input
-    choice = input("Do you want to [m]ow the law or [q]uit")
+    choice = input(f"Hi {player.name}! Do you want to [m]ow the law or [q]uit")
 
     # choice to mow
     if (choice == "m"):
-        pass
+        player.mow(lawn)
+
+
     # choice to upgrade
 
     # choice to quit

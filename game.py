56667d8811978877a73bb9c5cb74b8e363697c6f
Erick Valencia <evalencia0911@gmail.com>
2024-03-07 12:46:11 -0700

    psuedo code breakdown

---
+++ game.py
@@ -0,0 +1,30 @@
+# Build a landscaper game - psuedo code
+
+# Game state (Player dictionary)
+
+# Location - use this as a stretch to mow lawns in different places
+
+# Weapons list (How does the player level up)
+
+# Create the character CLASS
+
+    # define constructor
+
+    # define the action of mowing the lawn
+        # What do you earn from mowing the lawn and where does it tabulate your earnings
+
+# General functions
+
+    # Win condition
+
+    # Game set up 
+
+# Loop for the game
+
+    # Input loops
+
+    # choice to mow
+
+    # choice to upgrade
+
+    # choice to quit
